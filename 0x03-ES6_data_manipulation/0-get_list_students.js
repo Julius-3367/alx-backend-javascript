586249073dcf1e0209db9db04cd899a62ffa3da5
Julius-3367 <korirjulius001@gmail.com>
2024-04-29 23:19:18 -0700

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,21 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
+export default function getListStudents() {
+  const 01 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const 02 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const 03 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [01, 02, 03];
 }
-
-export default getListStudents;
-
